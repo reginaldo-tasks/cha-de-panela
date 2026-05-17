b78a7587ae1f8a424c3bd63738053dbbb7252af9
Reginaldo P. Carvalho <reginaldo.tasks@gmail.com>
2026-05-17 16:19:34 -0300

    fix: force DATABASE_URL in production, require it if not DEBUG

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -102,9 +102,9 @@ else:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
-                'NAME': BASE_DIR / 'db.sqlite3',
-            }
+            'NAME': BASE_DIR / 'db.sqlite3',
         }
+    }
 
 # Password validation
 AUTH_PASSWORD_VALIDATORS = [
